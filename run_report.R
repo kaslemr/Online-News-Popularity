42e597e7c0db928a42742645f65332a96ea58d97
mkasle <mkasle@ncsu.edu>
2020-10-16 17:09:23 -0500

    Added automation to file and create r script to run automation

---
+++ run_report.R
@@ -0,0 +1,8 @@
+library(rmarkdown)
+
+days_of_week = c("monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday")
+
+for (day in days_of_week){
+  rmarkdown::render("online_news_automated_reports.Rmd", output_file = paste0("files/",day,".html"), 
+                    params = list(day_of_week = day))
+}
